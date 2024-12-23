518452e0df5380a5de93eb93182cc20d3c442bdb
Vinay <53383503+vikuviku6666@users.noreply.github.com>
2024-12-23 09:47:32 +0100

    initial commit

---
+++ src/main/java/se/vinay/springbootstater/SpringBootStaterApplication.java
@@ -0,0 +1,15 @@
+package se.vinay.springbootstater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStaterApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringBootStaterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/vinay/springbootstater/SpringBootStaterApplicationTests.java
@@ -0,0 +1,15 @@
+package se.vinay.springbootstater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStaterApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
